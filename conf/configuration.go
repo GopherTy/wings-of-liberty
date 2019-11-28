f87b0a9986433225585c963417afa1bb70950295
GOpherTy <typrg951753@163.com>
2019-11-28 21:22:58 +0800

    add configuration object

--- conf/configuration.go
+++ conf/configuration.go
@@ -1,5 +1,32 @@
 package conf
 
-// Cnf configuration object
-type Cnf struct {
+import (
+	"log"
+
+	"go.uber.org/zap"
+)
+
+// Cfg configuration object
+type Cfg struct {
+	Logger *zap.Logger
+}
+
+var _Cfg Cfg
+
+// _Logger logger object
+var _Logger *zap.Logger
+
+//	initialize configuration object
+func init() {
+	_Logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("can't initialize logger:%v", err)
+	}
+	_Cfg.Logger = _Logger
+}
+
+// GetCfg get a logger
+func GetCfg() (cfg *Cfg) {
+	cfg = &_Cfg
+	return
 }
--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"Go_Overwall/conf"
 	"Go_Overwall/encryption"
-	"fmt"
 )
 
 func main() {
+	cfg := conf.GetCfg()
 	// test encode and decode
 	b := []byte{30, 25, 7, 8, 2}
 	encryArray := encryption.RandEncryArray()
 	cipher := encryption.NewCipher(encryArray)
 	cipher.Encode(b)
-	fmt.Println(b)
+	sugar := cfg.Logger.Sugar()
+	defer sugar.Sync()
+	sugar.Info(b)
 	cipher.Decode(b)
-	fmt.Println(b)
+	sugar.Info(b)
 }
